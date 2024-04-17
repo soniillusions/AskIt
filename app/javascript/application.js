c6a105769bf7cafdb3c10c1a3a017ce6babdc0bc
Aleksey <alekseycrosso@gmail.com>
2024-04-17 12:42:31 +0300

    Rails app Init

---
+++ app/javascript/application.js
@@ -0,0 +1,8 @@
+// Entry point for the build script in your package.json
+import * as bootstrap from "bootstrap"
+
+import Rails from "@rails/ujs"
+import Turbolinks from "turbolinks"
+
+Rails.start()
+Turbolinks.start()

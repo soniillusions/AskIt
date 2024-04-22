9075e65f2239045d808dc0ba6c806a364c32882b
Alexey Maryshenkov <alekseycrosso@gmail.com>
2024-04-22 19:21:51 +0300

    lesson 5, add Answer entity, add ErrorHandling module, add new answer form and show all answers on Question page

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,10 +1,13 @@
 class QuestionsController < ApplicationController
+  before_action :set_question!, only: [:show, :edit, :update, :destroy]
+
   def index
     @questions = Question.all
   end
 
   def show
-    @question = Question.find_by id: params[:id]
+    @answer = @question.answers.build
+    @answers = @question.answers.order(created_at: :desc)
   end
 
   def new
@@ -22,11 +25,9 @@ class QuestionsController < ApplicationController
   end
 
   def edit
-    @question = Question.find_by id: params[:id]
   end
 
   def update
-    @question = Question.find_by id: params[:id]
     if @question.update question_params
       flash[:success] = "Question updated!"
       redirect_to questions_path
@@ -36,7 +37,6 @@ class QuestionsController < ApplicationController
   end
 
   def destroy
-    @question = Question.find_by id: params[:id]
     @question.destroy
     flash[:success] = "Question deleted!"
     redirect_to questions_path
@@ -46,4 +46,9 @@ class QuestionsController < ApplicationController
   def question_params
     params.require(:question).permit(:title, :body)
   end
+
+  def set_question!
+    @question = Question.find params[:id]
+  end
 end
+
